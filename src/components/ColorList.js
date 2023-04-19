abd07d43a288aaf8a453d8f383d03db006a8a0ca
laura-paullette <laura.paullette@student.moringaschool.com>
2023-04-19 22:07:17 +0300

    completed assignment

--- src/components/ColorList.js
+++ src/components/ColorList.js
@@ -1,5 +1,7 @@
 import React from "react";
 
+
+
 function ColorList() {
   const colors = [
     "firebrick",
@@ -7,20 +9,37 @@ function ColorList() {
     "salmon",
     "darkslategray",
     "hotpink",
+    "red",
   ];
 
+
+  const colorElements = colors.map((color) => {
+    return <li key={color}style={{ color: color }}>{color}</li>;
+  });
   return (
     <div>
       <h1>Top 5 CSS Colors</h1>
-      <ol>
-        <li style={{ color: colors[0] }}>{colors[0]}</li>
-        <li style={{ color: colors[1] }}>{colors[1]}</li>
-        <li style={{ color: colors[2] }}>{colors[2]}</li>
-        <li style={{ color: colors[3] }}>{colors[3]}</li>
-        <li style={{ color: colors[4] }}>{colors[4]}</li>
+      <ol> 
+        {/* display the array of <li> elements here! */}
+        {colorElements}
       </ol>
     </div>
   );
 }
+//   return (
+//     <div>
+//       <h1>Top 5 CSS Colors</h1>
+//       <ol>
+//         <li style={{ color: colors[0] }}>{colors[0]}</li>
+//         <li style={{ color: colors[1] }}>{colors[1]}</li>
+//         <li style={{ color: colors[2] }}>{colors[2]}</li>
+//         <li style={{ color: colors[3] }}>{colors[3]}</li>
+//         <li style={{ color: colors[4] }}>{colors[4]}</li>
+//         <li style={{ color: colors[5] }}>{colors[5]}</li>
+
+//       </ol>
+//     </div>
+//   );
+// }
 
 export default ColorList;
